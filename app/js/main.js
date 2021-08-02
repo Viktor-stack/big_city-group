8d7bdaf0351a05577c7086d59be7162ec9a05de2
Viktor-stack <grohik493@gmail.com>
2021-08-02 13:40:06 +0300

    added mobile responsive

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 $(function () {
     $('select').styler();
+
+    $('.menu-btn').on('click', function (e) {
+        e.preventDefault();
+        $(".one").toggleClass('active')
+        $(".three").toggleClass('active')
+        $(".two").toggleClass('hidden')
+        $('.menu').toggleClass('menu_active');
+    })
+
 })
